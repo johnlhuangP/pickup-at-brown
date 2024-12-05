5688e794a4b61d97d72e073ad82b22b7990e2c0b
cerv171 <cpascual2005@gmail.com>
2024-12-04 21:38:17 -0500

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.routers import posts # Import the posts router
 
 app = FastAPI()
 
+# Include the posts router
+app.include_router(posts.router)
 
 @app.get("/")
 async def root():
