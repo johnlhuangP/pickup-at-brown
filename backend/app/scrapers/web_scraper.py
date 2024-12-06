e8b2cb30373f8b8d19f8e52b295a887e7b7653e8
johnlhuangP <john_huang@brown.edu>
2024-12-05 23:50:46 -0500

    Revert "test: see if i can push to main"
    This reverts commit b0a5a7af4591c3a70153851ea97b87ee573bb086.

--- backend/app/scrapers/web_scraper.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def scrape_example():
-    url = "https://example.com"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # Parse the data
-    return {"data": "example data"}
