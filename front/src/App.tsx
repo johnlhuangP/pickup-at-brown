1b9a6eb61b993a7f493262c722872c1033c3c438
Alex <alexander_n_tran@brown.edu>
2024-12-11 16:42:52 -0500

    Save local changes to App.tsx before pulling

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,21 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 // import Sidebar from './components/Sidebar';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@coreui/coreui/dist/css/coreui.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@coreui/coreui/dist/css/coreui.min.css";
 
 // Import the pages
- import HomePage from './pages/HomePage';
-// import AboutPage from './pages/AboutPage';
-// import ProfilePage from './pages/ProfilePage';
+import HomePage from "./pages/HomePage";
+
+import SignUpPage from "./pages/SignUpPage";
+import Register from "./pages/Register";
+
+import { SignIn } from "@clerk/clerk-react";
 
 function App() {
   return (
     <Router>
       <div className="app">
         {/* Header and Sidebar are present on all pages */}
-        <Header />        
+        <Header />
         {/* Main content section */}
         <div className="content">
           <Routes>
@@ -23,6 +26,9 @@ function App() {
             <Route path="/" element={<HomePage />} />
             {/* <Route path="/about" element={<AboutPage />} /> */}
             {/* <Route path="/profile" element={<ProfilePage />} /> */}
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<SignIn />} />
           </Routes>
         </div>
       </div>
